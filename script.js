2fb1b89263c69af296f3480641628d210578af4d
Arsen <keetrussia@mail.ru>
2020-10-22 18:02:28 +0300

    For test request

--- script.js
+++ script.js
@@ -38,65 +38,38 @@ Object.prototype.toStringURLParameters = function () {
     return keys.map(key => `${key}=${typeof this[key] == 'object' ? this[key].join() : this[key]}`).join('&')
 }
 
-function addScript(src) {
-    console.log(src)
-    var elem = document.createElement('script')
-    elem.src = src
-    document.head.appendChild(elem)
-    elem.onload = function () {
-        elem.remove()
+function request(method = '', parametrs = {}) {
+
+    // parametrs['access_token'] = 'b1f4136fdd0cbf2a0b750066a59707654a0a361df1b33fa6a98e2b459e806a341d6b8c05b1db38481c3a4'
+    parametrs['v'] = 5.52
+    parametrs['callback'] = 'response'
+    const src = `https://api.vk.com/method/${method}?${parametrs.toStringURLParameters()}`
+
+
+    return new Promise((resolve, reject) => {
+        function response(data) {
+            resolve(data)
+        }
+
+        const script = document.createElement('script')
+        script.src = src
+        document.head.appendChild(script)
+    })
+}
+
+if (document.location.search[0] = '?') {
+    let code = document.location.search.slice(1).split('&').map(i => i.split('=')).find(([key, _]) => key === 'code')
+    if (code) {
+        request('friends.get', { access_token: code[1], parametrscount: 10 }).then(response => console.log(response))
+    } else {
+        const authorizationParametr = {
+            client_id:  7636014,
+            redirect_uri: 'https://keet5.github.io/show-five-friends-from-vk/',
+            display: 'popup',
+            scope: 'friends',
+            revoke: 1
+        }
+
+        window.location.replace('https://oauth.vk.com/authorize?' + authorizationParametr.toStringURLParameters())
     }
 }
-
-function response(data) {
-    console.log(data)
-}
-
-async function request(method = '', parametrs = {}) {
-    const token = 'access_token=b1f4136fdd0cbf2a0b750066a59707654a0a361df1b33fa6a98e2b459e806a341d6b8c05b1db38481c3a4'
-    const version = 'v=5.52'
-    const url = `https://api.vk.com/method/${method}?${token}&${version}${parametrs.toStringURLParameters()}`
-
-    try {
-        const response = await fetch(url, {
-            headers: {
-                
-            }
-        })
-        // const data = await response.json()
-        // if (data.error) {
-        //     throw new Error(data.error.error_msg)
-        // } else {
-        //     return data.response
-        // }
-        return response
-    } catch (error) {
-        console.log(error.message)
-    }
-}
-request('friends.get').then(response => console.log(response))
-const access_token = 'b1f4136fdd0cbf2a0b750066a59707654a0a361df1b33fa6a98e2b459e806a341d6b8c05b1db38481c3a4'
-
-// addScript('https://api.vk.com/method/friends.get?' + {
-//     access_token,
-//     v: 5.52,
-//     callback: 'response'
-// }.toStringURLParameters())
-
-
-// if (document.location.search[0] = '?') {
-//     let parametrs = document.location.search.slice(1).split('&').map(i => i.split('=')).find(([key, _]) => key === 'code')
-//     if (parametrs) {
-//         console.log('success')
-//     } else {
-//         const authorizationParametr = {
-//             client_id:  7636014,
-//             redirect_uri: 'https://keet5.github.io/show-five-friends-from-vk/',
-//             display: 'popup',
-//             scope: 'friends',
-//             revoke: 1
-//         }
-
-//         window.location.replace('https://oauth.vk.com/authorize?' + authorizationParametr.toStringURLParameters())
-//     }
-// }
