d6b98101b0ebf532b47187aa5a414b855239d5fe
Arsen <keetrussia@mail.ru>
2020-11-03 17:01:23 +0300

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
-    <header class="header">
-        <div class="header_nav">
-            <a class="header_anchor" href="https://vk.com/feed">
+    <header class="header"></header>
+
+    <div class="users" v-if="users">
+        <nav class="nav">
+            <a class="nav_anchor" href="https://vk.com/feed">
                 <img
                     src="https://vk.com/images/svg_icons/ic_head_logo.svg"
                     alt=""
                 />
             </a>
-        </div>
-    </header>
-
-    <div class="users" v-if="users">
-        <User v-for="(information, id) in users" :key="id" :id="id" v-bind="information" />
+        </nav>
+        <User
+            v-for="(information, id) in users"
+            :key="id"
+            :id="id"
+            v-bind="information"
+        />
     </div>
-    <a :href="$authorizationLink" class="authorization_button" v-else>авторизоваться</a>
+    <a :href="$authorizationLink" class="authorization_button" v-else
+        >авторизоваться</a
+    >
 </template>
 
 <script>
@@ -27,12 +33,12 @@ export default {
 
     data() {
         return {
-            users: {}
+            users: {},
         };
     },
 
     async created() {
-        this.users = await this.$getUsers()
+        this.users = await this.$getUsers();
     },
 };
 </script>
@@ -50,20 +56,20 @@ body {
     background-color: #4a76a8;
     border-bottom: 1px #4a76a8;
     margin: 0;
+    position: absolute;
+    z-index: -1;
 }
 
-.header_nav {
-    width: 1305px;
-    margin: auto;
-    padding: 11px 0 0 15px;
-    box-sizing: border-box;
+.nav {
+    padding-top: 11px;
+    grid-column: 1 / -1;
 }
 
-.header_anchor {
+.nav_anchor {
     display: block;
 }
 
-.header_anchor:active {
+.nav_anchor:active {
     padding-top: 1px;
 }
 
@@ -73,21 +79,26 @@ body {
     border: none;
     border-radius: 4px;
     padding: 7px 20px 8px;
-    margin:   15px auto;
+    margin: 15px auto;
     display: block;
     font-size: 12.5px;
-    font-family: -apple-system,BlinkMacSystemFont,'Roboto','Helvetica Neue',Geneva,"Noto Sans Armenian","Noto Sans Bengali","Noto Sans Cherokee","Noto Sans Devanagari","Noto Sans Ethiopic","Noto Sans Georgian","Noto Sans Hebrew","Noto Sans Kannada","Noto Sans Khmer","Noto Sans Lao","Noto Sans Osmanya","Noto Sans Tamil","Noto Sans Telugu","Noto Sans Thai",sans-serif,arial,Tahoma,verdana;
+    font-family: -apple-system, BlinkMacSystemFont, "Roboto", "Helvetica Neue",
+        Geneva, "Noto Sans Armenian", "Noto Sans Bengali", "Noto Sans Cherokee",
+        "Noto Sans Devanagari", "Noto Sans Ethiopic", "Noto Sans Georgian",
+        "Noto Sans Hebrew", "Noto Sans Kannada", "Noto Sans Khmer",
+        "Noto Sans Lao", "Noto Sans Osmanya", "Noto Sans Tamil",
+        "Noto Sans Telugu", "Noto Sans Thai", sans-serif, arial, Tahoma, verdana;
     line-height: 15px;
     display: block;
     width: 100px;
     text-align: center;
     user-select: none;
-  
+
     text-decoration: none;
 }
 
 .authorization_button:hover {
-    opacity: .88
+    opacity: 0.88;
 }
 
 .authorization_button:active {
@@ -96,55 +107,14 @@ body {
     padding: 8px 20px 7px;
 }
 .users {
-    width: 1305px;
+    max-width: 1275px;
     display: grid;
     margin: auto;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 200px));
+    grid-template-columns: repeat(auto-fill, 200px);
+    grid-template-rows: 42px;
+    justify-content: center;
+    align-items: stretch;
     grid-gap: 15px;
-    padding: 15px;
-    align-items: center;
     box-sizing: border-box;
-}
-
-@media (max-width: 1304px) and (min-width: 1090px) {
-    .users,
-    .header_nav {
-        width: 1090px;
-    }
-}
-
-@media (max-width: 1089px) and (min-width: 875px) {
-    .users,
-    .header_nav {
-        width: 875px;
-    }
-}
-
-@media (max-width: 874px) and (min-width: 660px) {
-    .users,
-    .header_nav {
-        width: 660px;
-    }
-}
-
-@media (max-width: 659px) and (min-width: 445px) {
-    .users,
-    .header_nav {
-        width: 445px;
-    }
-}
-
-@media (max-width: 444px) and (min-width: 230px) {
-    .users,
-    .header_nav {
-        width: 230px;
-    }
-}
-
-@media (max-width: 229px) {
-    .users,
-    .header_nav {
-        width: 230px;
-    }
 }
 </style>
